efe53a1f9a3db04f750a6efbd1b3580edaaf5feb
c-richard <richardcarruthers8@gmail.com>
2021-02-21 13:14:40 +0000

    de-occupy nodes on creep death

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,35 @@ import "augments/spawn";
 import "augments/creep";
 import "augments/roomPosition";
 
+function cleanupCreep(creepName: string) {
+  const occupyingNodeId = Memory.creeps[creepName].occupying?.id;
+  if (occupyingNodeId) {
+    // de occupy node
+    Object.values(Memory.rooms).forEach((r) => {
+      if (r.nodeById[occupyingNodeId] != null) {
+        r.nodeById[occupyingNodeId].occupiedBy = null;
+      }
+    });
+  }
+
+  // clear memory;
+  delete Memory.creeps[creepName];
+}
+
 const loop = () => {
   Object.values(Game.rooms).map((r) => {
     r.visualise();
   });
 
-  Object.values(Game.creeps).map((c) => {
-    c.visualise();
-    c.update();
+  Object.keys(Memory.creeps).map((creepName) => {
+    const creep = Game.creeps[creepName];
+
+    if (creep != null) {
+      creep.visualise();
+      creep.update();
+    } else {
+      cleanupCreep(creepName);
+    }
   });
 };
 
